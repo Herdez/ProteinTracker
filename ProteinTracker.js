f7cc714e31858302c4f7cfe3b88c17f20d1a37aa
Herdez <Herdez@gmail.com>
2015-01-28 20:16:36 -0600

    Adding-data-template

--- ProteinTracker.js
+++ ProteinTracker.js
@@ -1,5 +1,13 @@
 if (Meteor.isClient) {
-  
+  Template.userDetails.helpers({
+    user: function() {
+       var user = {
+         total: 123,
+         goal: 251
+       };
+       return user;
+    }
+  });
 }
 
 if (Meteor.isServer) {
