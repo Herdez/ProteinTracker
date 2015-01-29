eead86faaf72dff251044ab11e334be54c76c1c7
Herdez <Herdez@gmail.com>
2015-01-28 21:28:11 -0600

    Adding-history-templates

--- ProteinTracker.js
+++ ProteinTracker.js
@@ -8,6 +8,21 @@ if (Meteor.isClient) {
        return user;
     }
   });
+
+  Template.history.helpers({
+    historyItem: function() {
+      var historyItems =
+       [
+          { date: '10/31/2013 5:00 AM', value: 20 },
+          { date: '10/31/2013 6:00 AM', value: 25 },
+          { date: '10/31/2013 7:00 AM', value: 10 },
+          { date: '10/31/2013 8:00 AM', value: 5 },
+          { date: '10/31/2013 9:00 AM', value: 20 }
+          
+       ];
+       return historyItems;
+    }
+  });
 }
 
 if (Meteor.isServer) {
